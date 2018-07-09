f14dff144c77bde9a57843fff4716fb6f8d24a1d
rp-cko <christopher.kopic@regiepapst.de>
2018-07-09 16:03:04 +0200

    Added support for multiple polyfits

--- __main__.py
+++ __main__.py
@@ -1,5 +1,4 @@
-import sys
-import os
+import sys, os, warnings
 from datetime import datetime
 
 from PyQt5 import QtWidgets, QtGui, QtCore
@@ -19,8 +18,7 @@ class GraphWindow(QtWidgets.QWidget):
         self.modTimes = []
         self.frameNums = []
         self.frameNumsTotal = 200
-        self.polylevel = 2
-        self.polyfitTimes = []
+        self.polyfits = {}
         self.timeFinished = None
 
         self.initUI()
@@ -53,29 +51,35 @@ class GraphWindow(QtWidgets.QWidget):
 
         subgrid_lvls = QtWidgets.QGridLayout()
 
-        chk_lvl1 = QtWidgets.QCheckBox("1", self)
-        chk_lvl1.resize(chk_lvl1.sizeHint())
-        subgrid_lvls.addWidget(chk_lvl1,0,0)
+        self.chk_lvl1 = QtWidgets.QCheckBox("1", self)
+        self.chk_lvl1.resize(self.chk_lvl1.sizeHint())
+        self.chk_lvl1.stateChanged.connect(self.update_deg1)
+        subgrid_lvls.addWidget(self.chk_lvl1,0,0)
 
-        chk_lvl2 = QtWidgets.QCheckBox("2", self)
-        chk_lvl2.resize(chk_lvl2.sizeHint())
-        subgrid_lvls.addWidget(chk_lvl2,0,1)
+        self.chk_lvl2 = QtWidgets.QCheckBox("2", self)
+        self.chk_lvl2.resize(self.chk_lvl2.sizeHint())
+        self.chk_lvl2.stateChanged.connect(self.update_deg2)
+        subgrid_lvls.addWidget(self.chk_lvl2,0,1)
 
-        chk_lvl3 = QtWidgets.QCheckBox("3", self)
-        chk_lvl3.resize(chk_lvl3.sizeHint())
-        subgrid_lvls.addWidget(chk_lvl3,0,2)
+        self.chk_lvl3 = QtWidgets.QCheckBox("3", self)
+        self.chk_lvl3.resize(self.chk_lvl3.sizeHint())
+        self.chk_lvl3.stateChanged.connect(self.update_deg3)
+        subgrid_lvls.addWidget(self.chk_lvl3,0,2)
 
-        chk_lvl4 = QtWidgets.QCheckBox("4", self)
-        chk_lvl4.resize(chk_lvl4.sizeHint())
-        subgrid_lvls.addWidget(chk_lvl4,1,0)
+        self.chk_lvl4 = QtWidgets.QCheckBox("4", self)
+        self.chk_lvl4.resize(self.chk_lvl4.sizeHint())
+        self.chk_lvl4.stateChanged.connect(self.update_deg4)
+        subgrid_lvls.addWidget(self.chk_lvl4,1,0)
 
-        chk_lvl5 = QtWidgets.QCheckBox("5", self)
-        chk_lvl5.resize(chk_lvl5.sizeHint())
-        subgrid_lvls.addWidget(chk_lvl5,1,1)
+        self.chk_lvl5 = QtWidgets.QCheckBox("5", self)
+        self.chk_lvl5.resize(self.chk_lvl5.sizeHint())
+        self.chk_lvl5.stateChanged.connect(self.update_deg5)
+        subgrid_lvls.addWidget(self.chk_lvl5,1,1)
 
-        chk_lvl6 = QtWidgets.QCheckBox("6", self)
-        chk_lvl6.resize(chk_lvl6.sizeHint())
-        subgrid_lvls.addWidget(chk_lvl6,1,2)
+        self.chk_lvl6 = QtWidgets.QCheckBox("6", self)
+        self.chk_lvl6.resize(self.chk_lvl6.sizeHint())
+        self.chk_lvl6.stateChanged.connect(self.update_deg6)
+        subgrid_lvls.addWidget(self.chk_lvl6,1,2)
 
         grid.addLayout(subgrid_lvls,3,0,1,1)
 
@@ -83,7 +87,7 @@ class GraphWindow(QtWidgets.QWidget):
 
         spi_numFrames = QtWidgets.QSpinBox(self, minimum = 1, maximum = 1000000, value = 200)
         spi_numFrames.resize(spi_numFrames.sizeHint())
-        # spi_numFrames.valueChanged.connect(self.update_framenumstotal)
+        spi_numFrames.valueChanged.connect(self.update_framenumstotal)
         subgrid_frames.addWidget(spi_numFrames,0,0,1,1)
 
         spi_in = QtWidgets.QSpinBox(self, minimum = 0, maximum = 1000000, value = 0)
@@ -118,7 +122,13 @@ class GraphWindow(QtWidgets.QWidget):
         if filepaths != None:
             self.lab_dispFilepath.setText(str(filepaths[0]))
             self.calc_modtimes(filepaths)
-            self.polyfitTimes = []
+            self.polyfits.clear()
+            self.chk_lvl1.setChecked(False)
+            self.chk_lvl2.setChecked(False)
+            self.chk_lvl3.setChecked(False)
+            self.chk_lvl4.setChecked(False)
+            self.chk_lvl5.setChecked(False)
+            self.chk_lvl6.setChecked(False)
             self.update_plot()
         else:
             pass
@@ -147,31 +157,38 @@ class GraphWindow(QtWidgets.QWidget):
         self.modTimesUnix.sort()
         self.modTimes.sort()
 
-    def create_polyfit(self):
+    def create_polyfit(self, deg):
         if len(self.frameNums) <= 0 or len(self.frameNums) != len(self.modTimesUnix):
             print("Not computable")
             return None
 
         x = np.array(self.frameNums)
         y = np.array(self.modTimesUnix)
-        p1 = np.polyfit(x,y,self.polylevel)
+        with warnings.catch_warnings():
+            warnings.filterwarnings('error')
+            try:
+                p1 = np.polyfit(x,y,deg)
+            except np.RankWarning:
+                print("Not enough data")
+                return None
 
         polyfitTimesUnix = np.polyval(p1,range(self.frameNumsTotal))
-        self.polyfitTimes = []
+        polyfitTimes = []
         for timeU in polyfitTimesUnix:
-            self.polyfitTimes.append(datetime.fromtimestamp(timeU))
-        self.timeFinished = datetime.fromtimestamp(int(polyfitTimesUnix[self.frameNumsTotal-1]))
-
-        self.update_plot()
+            polyfitTimes.append(datetime.fromtimestamp(timeU))
+        self.polyfits.update({deg: polyfitTimes})
+        #self.timeFinished = datetime.fromtimestamp(int(polyfitTimesUnix[self.frameNumsTotal-1]))
 
     def update_plot(self):
         self.fig.clf()
         ax = self.fig.add_subplot(111, label='babo')
         ax.set(xlabel='Frame', ylabel='Creation Date', title='Render Progress')
 
-        if len(self.polyfitTimes) > 0:
-            ax.plot(range(len(self.polyfitTimes)),self.polyfitTimes, label="approximation")
-            ax.text(self.frameNumsTotal-1, self.timeFinished, "Finished approx.: " + str(self.timeFinished), fontsize=10, horizontalalignment="right")
+        if len(self.polyfits) > 0:
+            for deg, timeList in self.polyfits.items():
+                if len(timeList) > 0:
+                    ax.plot(range(len(timeList)),timeList, label="Deg.:"+str(deg))
+                    #ax.text(self.frameNumsTotal-1, self.timeFinished, "Finished approx.: " + str(self.timeFinished), fontsize=10, horizontalalignment="right")
 
         if len(self.modTimes) > 0 and len(self.modTimes)==len(self.frameNums):
             ax.plot(self.frameNums, self.modTimes, '+', label='data')
@@ -180,11 +197,61 @@ class GraphWindow(QtWidgets.QWidget):
 
         self.canv.draw()
 
+    def update_deg1(self):
+        if self.chk_lvl1.isChecked():
+            self.create_polyfit(1)
+        else:
+            self.polyfits.pop(1, None)
+        self.update_plot()
+
+    def update_deg2(self):
+        if self.chk_lvl2.isChecked():
+            self.create_polyfit(2)
+        else:
+            self.polyfits.pop(2, None)
+        self.update_plot()
+
+    def update_deg3(self):
+        if self.chk_lvl3.isChecked():
+            self.create_polyfit(3)
+        else:
+            self.polyfits.pop(3, None)
+        self.update_plot()
+
+    def update_deg4(self):
+        if self.chk_lvl4.isChecked():
+            self.create_polyfit(4)
+        else:
+            self.polyfits.pop(4, None)
+        self.update_plot()
+
+    def update_deg5(self):
+        if self.chk_lvl5.isChecked():
+            self.create_polyfit(5)
+        else:
+            self.polyfits.pop(5, None)
+        self.update_plot()
+
+    def update_deg6(self):
+        if self.chk_lvl6.isChecked():
+            self.create_polyfit(6)
+        else:
+            self.polyfits.pop(6, None)
+        self.update_plot()
+
     def update_framenumstotal(self, val):
         self.frameNumsTotal = val
-
-    def update_polylevel(self, val):
-        self.polylevel = val
+        self.polyfits.clear()
+        degs = []
+        if self.chk_lvl1.isChecked(): degs.append(1)
+        if self.chk_lvl2.isChecked(): degs.append(2)
+        if self.chk_lvl3.isChecked(): degs.append(3)
+        if self.chk_lvl4.isChecked(): degs.append(4)
+        if self.chk_lvl5.isChecked(): degs.append(5)
+        if self.chk_lvl6.isChecked(): degs.append(6)
+        for deg in degs:
+            self.create_polyfit(deg)
+        self.update_plot()
 
     def center(self):
         qr = self.frameGeometry()
